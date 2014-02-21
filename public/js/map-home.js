027de323cc74add0528c27dc227452b35c709f52
Scooterlicious <sfell2006@gmail.com>
2014-02-21 15:39:08 -0500

    fix globe size

--- public/js/map-home.js
+++ public/js/map-home.js
@@ -32,7 +32,7 @@ var width = parseInt(d3.select('body').style('width')),
 
 var projection = d3.geo.orthographic()
   .translate([width / 2, height / 2])
-  .scale(340)
+  .scale(300)
   .precision(.1)
   .clipAngle(90)
 
